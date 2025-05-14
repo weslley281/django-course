f94d239b7f2b0a64dd51ca5213806e2254c6da64
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2025-05-14 09:44:17 -0400

    a

--- challenges/views.py
+++ challenges/views.py
@@ -2,6 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
+def monthly_challenge_by_number(request, month):
+    challenge_text = None
+
+    if month == 1:
+        challenge_text = "Isso está funcionando em janeiro"
+    elif month == 2:
+        challenge_text = "Isso está funcionando em fevereiro"
+    elif month == 3:
+        challenge_text = "Isso está funcionando em março"
+    else:
+        return HttpResponseNotFound("404 Página não encontrada")
+
+    return HttpResponse(challenge_text)
+
+
 def monthly_challenge(request, month):
     challenge_text = None
 
@@ -12,6 +27,6 @@ def monthly_challenge(request, month):
     elif month == 'march':
         challenge_text = "Isso está funcionando em março"
     else:
-        return HttpResponseNotFound("Isso não é suportado")
+        return HttpResponseNotFound("404 Página não encontrada")
 
     return HttpResponse(challenge_text)
